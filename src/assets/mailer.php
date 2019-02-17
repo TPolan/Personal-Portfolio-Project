abda217ea47d916c113479001240c2b1fd70c5fb
TPolan <t.poly@seznam.cz>
2019-02-18 00:13:34 +0100

    done and done

---
+++ src/assets/mailer.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    // The message
+    $message = $_POST['message'];
+    
+    // In case any of our lines are larger than 70 characters, we should use wordwrap()
+    $message = wordwrap($message, 70, "\r\n");
+    
+    // Send
+    mail('t.poly@seznam.cz', "{$_POST['first-name']} {$_POST['last-name']} {$_POST['phone']}", $message);
+    
+    
+    header("Location: /");
+    exit();
